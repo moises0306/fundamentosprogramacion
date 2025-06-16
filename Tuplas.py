43689861b0f868a219dced3eff9163d76341777e
POAPCA02LC0115 <POAPCA01LC0400@POAPCA02LC0115.duoc.acad>
2025-06-16 14:12:38 -0400

    segundo proyecto

---
+++ Tuplas.py
@@ -0,0 +1,4 @@
+mi_lista = [1, 2, 3]
+mi_tupla = tuple(mi_lista)
+
+print(mi_tupla)
---
+++ conjunto.py
@@ -0,0 +1,7 @@
+conjunto = {3, 1, 4, 1, 5, 2, 4}
+lista= {3, 1, 4, 1, 5, 2, 4}
+
+print("conjunto")
+print(conjunto)
+print("lista")
+print(lista)
